214f57d61416934da36b2b8c8047b983f170175b
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-08-01 20:40:07 +0100

    refactor: type locale package build configs

---
+++ packages/locale/rollup-build-packages-config.ts
@@ -0,0 +1,68 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import virtual from '@rollup/plugin-virtual';
+import type { RollupOptions } from 'rollup';
+import { mergeConfig } from '../../shared/rollup-config.ts';
+import { defaultConfig, plugins } from './rollup.config.ts';
+import renderCldrDataLoader from './utils/clrdr-data-render.ts';
+
+export type LocalePackages = Readonly<Record<string, readonly string[]>>;
+
+export interface LocaleBuild {
+    readonly destDir: string;
+    readonly packages: LocalePackages;
+}
+
+export function buildRollupConfig(options: { readonly locales: readonly string[] }): RollupOptions {
+    const source = renderCldrDataLoader(options.locales);
+    return mergeConfig(defaultConfig, {
+        plugins: [
+            plugins.babel,
+            virtual({ 'cldr-entry': source }),
+            json(),
+            nodeResolve(),
+            commonjs(),
+        ],
+        input: 'cldr-entry',
+        output: {
+            name: 'JSJodaLocale',
+            globals: {
+                '@js-joda/core': 'JSJoda',
+                '@js-joda/timezone': 'JSJodaTimezone',
+                '@js-joda/locale': 'JSJodaLocale',
+            },
+        },
+        external: ['@js-joda/core', '@js-joda/timezone', '@js-joda/locale'],
+    });
+}
+
+export function buildRollupConfigs(options: LocaleBuild): RollupOptions[] {
+    return Object.entries(options.packages).flatMap(([name, locales]) => {
+        const config = buildRollupConfig({ locales });
+        return [
+            mergeConfig(config, {
+                output: {
+                    file: `${options.destDir}/${name}/index.js`,
+                    format: 'umd',
+                    sourcemap: true,
+                },
+            }),
+            mergeConfig(config, {
+                output: {
+                    file: `${options.destDir}/${name}/index.esm.js`,
+                    format: 'es',
+                    sourcemap: true,
+                },
+            }),
+            mergeConfig(config, {
+                plugins: [...(config.plugins ?? []), plugins.minify],
+                output: {
+                    file: `${options.destDir}/${name}/index.min.js`,
+                    format: 'iife',
+                    sourcemap: false,
+                },
+            }),
+        ];
+    });
+}
