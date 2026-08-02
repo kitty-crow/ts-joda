c1d83608929bc719c925208e17166da7426ec6bb
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-08-02 15:21:51 +0100

    refactor(extra): migrate Temporals to TypeScript

--- packages/extra/src/math.ts
+++ packages/extra/src/math.ts
@@ -3,6 +3,7 @@ import * as core from '@js-joda/core';
 interface MathOps {
     intDiv(x: number, y: number): number;
     intMod(x: number, y: number): number;
+    safeMultiply(x: number, y: number): number;
     safeToInt(value: number): number;
 }
 
